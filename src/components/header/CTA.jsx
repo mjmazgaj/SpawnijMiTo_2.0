be40249cdce48301142825a002598b9fae0548be
Mazgaj <>
2024-01-10 21:41:28 +0100

    Header : Desing | Add Shop button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,6 +5,7 @@ const CTA = () => {
     <div className="cta">
         <a href="#portfolio" className="btn">Realizacje</a>
         <a href="#contact" className="btn btn-primary">Kontakt</a>
+        <a href="/shop" className="btn btn-primary">Shop</a>
     </div>
   )
 }

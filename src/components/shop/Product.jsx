a0e45fa14c56a8fa3e95816f3f0b928b5b3290cc
Mazgaj <>
2024-01-11 17:03:58 +0100

    ContextAPI : Demo | Add clickContext to Product component and display + and - buttons

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -1,9 +1,18 @@
+import { useClicks } from "./ClicksContext";
+
 const Product = ({ id, image, name, price }) => {
+  const { incrementClicks, decrementClicks } = useClicks();
+
   return (
     <ul className="product-container" key={id}>
       <img src={image} alt="" />
       <h3>{name}</h3>
       <p>Cena: {price}</p>
+      
+      <div className="product-Clicks__buttons">
+        <button onClick={incrementClicks} className='btn'>+</button>
+        <button onClick={decrementClicks} className='btn'>-</button>
+      </div>
     </ul>
   );
 };

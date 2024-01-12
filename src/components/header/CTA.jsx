0939c2ec7cd20eb73c275e771724ac9d1f12c341
Mazgaj <>
2024-01-12 17:23:24 +0100

    Header: design | Remove portfolio link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,6 @@ import "./header.css";
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="#portfolio" className="btn">Realizacje</a>
         <a href="#contact" className="btn btn-primary">Kontakt</a>
         <a href="/shop" className="btn btn-primary">Shop</a>
     </div>

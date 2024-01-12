ebbe98ad68646acd6ddf6f2fb63b7e1d96b7606b
Mazgaj <>
2024-01-13 00:38:58 +0100

    Shop: Design | Change + and - buttons style in SummaryProductItem

--- src/components/shop/SummaryProductItem.jsx
+++ src/components/shop/SummaryProductItem.jsx
@@ -1,4 +1,6 @@
 import { useProducts } from "./ProductsContext";
+import { CiSquarePlus } from "react-icons/ci";
+import { CiSquareMinus } from "react-icons/ci";
 
 const SummaryProductItem = ({ id, product }) => {
   const { addItem, removeItem, summaryProductList } = useProducts();
@@ -10,13 +12,9 @@ const SummaryProductItem = ({ id, product }) => {
         <p>{product.Name}</p>
 
         <div className="summary-product-item__buttons">
-          <button onClick={() => addItem(product)} className="btn">
-            +
-          </button>
+          <CiSquarePlus className="summary-product-item__button" onClick={() => addItem(product)}/>
           <p className="summary-product-item__quantity">Ilość: {product.quantity}</p>
-          <button onClick={() => removeItem(product)} className="btn">
-            -
-          </button>
+          <CiSquareMinus className="summary-product-item__button" onClick={() => removeItem(product)}/>
         </div>
         <p className="summary-product-item__cost">Koszt: {product.cost.toFixed(2)} zł</p>
       </div>

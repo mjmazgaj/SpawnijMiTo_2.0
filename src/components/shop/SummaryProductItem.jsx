cfd346d6da6f9f8ea52fe2d34c4c7695117603d8
Mazgaj <>
2024-01-12 23:31:24 +0100

    Shop: Summary | Add SummaryProductItem structure

--- src/components/shop/SummaryProductItem.jsx
+++ src/components/shop/SummaryProductItem.jsx
@@ -5,6 +5,21 @@ const SummaryProductItem = ({ id, product }) => {
 
   return (
     <ul id={id} className="summary-product-item__container">
+      <img src={product.Image} alt="" />
+      <div className="summary-product-item__details">
+        <p>{product.Name}</p>
+
+        <div className="summary-product-item__buttons">
+          <button onClick={() => addItem(product)} className="btn">
+            +
+          </button>
+          <p className="summary-product-item__quantity">Ilość: {product.quantity}</p>
+          <button onClick={() => removeItem(product)} className="btn">
+            -
+          </button>
+        </div>
+        <p className="summary-product-item__cost">Koszt: {product.cost.toFixed(2)} zł</p>
+      </div>
     </ul>
   );
 };
